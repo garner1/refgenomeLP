9584a951eca61f51325ceb722fde0e4382f0d139
garner1 <silvano.garnerone@gmail.com>
2018-05-18 09:30:20 +0200

    remodularization of the pipeline

--- main.sh
+++ main.sh
@@ -4,25 +4,9 @@ bamfile=$1
 datadir=$2
 cutsite=$3
 
-# mkdir -p $datadir
-# input: bamfile from restseq
-# output: fasta file with expanded reference fragment
-# cd $datadir
-# bam2bed < $bamfile | cut -f-3 | awk '{OFS="\t"}{print $1,$2-50,$3+50}' | awk '$2>0' | sed 's/^chr//' | # enlarge the location of the mapped read by 50bp per side
-#     bedtools getfasta -fi ~/Work/genomes/Homo_sapiens.GRCh37.dna.primary_assembly.fa/GRCh37.fa -bed - | # generate the fasta file 
-#     paste - - | tr -d '>' | tr ':' '\t' | awk '{print $3>$1}'	# only keep read content splitted by chromosome
-# mkdir -p $datadir/chr
-# mv `seq 1 22` chr
-# Tokenize the genome documents 
-# echo "Tokenize the documents"
-# g++ -std=c++11 ~/Work/pipelines/genomicNLP/module/tokenizer_withMean.cpp -o ~/Work/pipelines/genomicNLP/module/mean # compile the tokenizer
-# mkdir -p $datadir/docs
-# cd $datadir/chr
-# parallel "~/Work/pipelines/genomicNLP/module/mean {} ~/Work/dataset/genomicNLP/CATG/6mer/{.}.table.tsv | cut -d' ' -f2- > ../docs/{.}.doc" ::: *
-# cat $datadir/docs/{?,??}.doc > $datadir/docs/genome.doc
+bash /home/garner1/Work/pipelines/refgenomeLP/generate_vocabulary.sh $bamfile $datadir $cutsite # generate vocabulary from dataset
+if [ ! -f $datadir/common_vocabulary.p ]; then
+    python /home/garner1/Work/pipelines/refgenomeLP/find_common_vocabulary.py # find intersection vocabulary
+fi
+python /home/garner1/Work/pipelines/refgenomeLP/build_PPMI_onCommonVoc.py $datadir/docs/genome.doc # truncated SVD of dtm matrix restricted to common vocabulary
 
-python /home/garner1/Work/pipelines/refgenomeLP/build_vocabulary.py $datadir/docs/genome.doc
-mv $datadir/docs/genome.doc.vocabulary.p $datadir/pickle_vocabularies
-
-# parallel --memfree 30G "python /home/garner1/Work/pipelines/refgenomeLP/vectorization.py {}" ::: /home/garner1/Work/dataset/refgenomeLP/XZ*/docs/genome.doc # process genome all together
-# parallel --memfree 30G "python /home/garner1/Work/pipelines/refgenomeLP/vectorization.py {}" ::: $datadir/docs/*.doc # process chromosomes separately
